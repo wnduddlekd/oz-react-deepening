19aae687d5ef79b3fbf151b9e79a6d52cc4e4520
wndud <ju03209@naver.com>
2025-05-22 17:35:48 +0900

    feat: 장바구니 결제 시스템 추가

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,33 +1,39 @@
 import React, { useState } from 'react';
+import ProductList from './ProductList';
 
 const Cart = ({ cart, setCart }) => {
     const [isOpen, setIsOpen] = useState(true);
 
     //TODO: 장바구니를 열고 닫는 함수를 완성하세요.
     //setIsOpen 함수를 사용하여 State값을 업데이트하세요.
-    const toggleCart = () => {};
+    const toggleCart = (setIsOpen) => {
+        setIsOpen((prev) => !prev);
+    };
 
     //TODO: cart에 담긴 상품의 총 가격을 계산하세요.
     //reduce 함수를 사용하여 총 가격을 계산하세요.
-    const totalPrice = 0;
+    const totalPrice = cart.reduce((acc, item) => acc + item.price, 0);
 
     //TODO: (선택 과제) 상품 가격에 서식을 지정하세요.
     //상품 가격을 3자리마다 콤마(,)를 찍어주는 함수를 작성하세요.
     //예시) 1000000 => 1,000,000
     //이 함수의 사용법은 totalPriceFormatted(가격) 입니다.
     //HINT : https://shape-coding.tistory.com/72
-    const totalPriceFormatted = () => {};
+    const totalPriceFormatted = () => totalPrice.toLocaleString();
 
     //TODO: 결제하기 버튼 구현하기
     //결제하기 버튼을 눌렀을 때 alert를 띄우고
     //장바구니를 비워주세요.
-    const handlePurchase = () => {};
+    const handlePurchase = () => {
+        alert('결제되었습니다.');
+        return setCart([]);
+    };
 
     return (
         <div className="cart">
             <div className="cart-header">
                 <h2>장바구니</h2>
-                <div onClick={toggleCart}>{isOpen ? '▲' : '▼'}</div>
+                <div onClick={() => toggleCart(setIsOpen)}>{isOpen ? '▲' : '▼'}</div>
             </div>
             {cart.length === 0 ? (
                 <p>장바구니가 비어있습니다.</p>
